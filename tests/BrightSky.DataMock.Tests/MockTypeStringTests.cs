7ebbad40b5ed195188ceca7fde5209e2232bb5d0
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-07-28 11:40:20 +0100

    Adding more unit tests from MockTypeString.

--- tests/BrightSky.DataMock.Tests/MockTypeStringTests.cs
+++ tests/BrightSky.DataMock.Tests/MockTypeStringTests.cs
@@ -17,4 +17,30 @@ public class MockTypeStringTests
 
         Assert.IsType<MockTypeString>(actual);
     }
+    
+    [Fact]
+    public void When_StringsGet_Returns_String()
+    {
+        var actual = Dm.Strings().Get();
+
+        Assert.IsType<string>(actual);
+    }
+    
+    [Fact]
+    public void When_Strings_Default_Length_Returns_DefaultLength()
+    {
+        var actual = Dm.Strings();
+
+        Assert.Equal(10, actual.Length);
+        Assert.IsType<MockTypeString>(actual);
+    }
+        
+    [Fact]
+    public void When_StringsGet_Default_Length_Returns_String_DefaultLength()
+    {
+        var actual = Dm.Strings().Get();
+
+        Assert.Equal(10, actual.Length);
+        Assert.IsType<string>(actual);
+    }
 }
