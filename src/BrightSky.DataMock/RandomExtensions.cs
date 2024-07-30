6052eed8ac2e41845dabbb073c317db57815e084
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-07-30 11:13:37 +0100

    Split HelperExtension file into its separate concern. ListExtensions and RandomExtensions. Still need to add unit tests to get full coverage.

--- src/BrightSky.DataMock/RandomExtensions.cs
+++ src/BrightSky.DataMock/RandomExtensions.cs
@@ -1,22 +1,7 @@
 ﻿namespace BrightSky.DataMock;
 
-// TODO: Refactor this eventually ... feels like 2 separate concerns ???
-internal static class HelperExtensions
+internal static class RandomExtensions
 {
-    public static List<T> Shuffle<T>(this List<T> list)
-    {
-        var random = new Random();
-        var n = list.Count;
-        while (n > 1)
-        {
-            n--;
-            var k = random.Next(n + 1);
-            (list[k], list[n]) = (list[n], list[k]);
-        }
-
-        return list;
-    }
-    
     public static short NextShort(this Random random, short minValue, short maxValue)
     {
         if (random is null)
