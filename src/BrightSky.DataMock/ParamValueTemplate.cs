382a3e1b06ad95be3d9e95e9693c737ccff69534
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-08-09 09:11:54 +0100

    Adding unit tests to get coverage of ToList(...) extension and some tweaks. All unit tests pass.

--- src/BrightSky.DataMock/ParamValueTemplate.cs
+++ src/BrightSky.DataMock/ParamValueTemplate.cs
@@ -8,7 +8,7 @@ public readonly record struct ParamValue
     public string Name { get; }
     public Func<object> MockTypeFactory { get; }
 
-    internal ParamValue(string name, Func<object> mockTypeFactory) => (Name, MockTypeFactory) = (name, mockTypeFactory);
+    public ParamValue(string name, Func<object> mockTypeFactory) => (Name, MockTypeFactory) = (name, mockTypeFactory);
 
     public string Get()
     {
@@ -27,6 +27,7 @@ internal readonly record struct ParamValueTemplate
 {
     private readonly List<ParamValue> _paramsValues = [];
     private readonly string _template;
+    
     internal ParamValueTemplate(string template)
     {
         if (string.IsNullOrWhiteSpace(template))
@@ -58,7 +59,7 @@ internal readonly record struct ParamValueTemplate
             foreach (Match match in matches)
             {
                 var t = generatedValues[match.Value].GetType();
-                var gta = t.GenericTypeArguments[0].FullName;
+                var gta = t.GenericTypeArguments[0].ToString();
                 formatted[i] = Format(gta, t, formatted[i], match.Value, generatedValues, i);
             }
         }
@@ -83,7 +84,7 @@ internal readonly record struct ParamValueTemplate
     private static string Format(string? gta, Type t, string formatted, string paramName, Dictionary<string, object> generatedValues, int i)
         => gta switch
         {
-            "System.Bool" => WhenListOf(t, formatted, paramName, ((List<bool>)generatedValues[paramName])[i]),
+            "System.Boolean" => WhenListOf(t, formatted, paramName, ((List<bool>)generatedValues[paramName])[i]),
             "System.Byte" => WhenListOf(t, formatted, paramName, ((List<byte>)generatedValues[paramName])[i]),
             "System.Int16" => WhenListOf(t, formatted, paramName, ((List<short>)generatedValues[paramName])[i]),
             "System.Int32" => WhenListOf(t, formatted, paramName, ((List<int>)generatedValues[paramName])[i]),
@@ -91,8 +92,9 @@ internal readonly record struct ParamValueTemplate
             "System.Single" => WhenListOf(t, formatted, paramName, ((List<float>)generatedValues[paramName])[i]),
             "System.Double" => WhenListOf(t, formatted, paramName, ((List<double>)generatedValues[paramName])[i]),
             "System.Decimal" => WhenListOf(t, formatted, paramName, ((List<decimal>)generatedValues[paramName])[i]),
+            "System.Char" => WhenListOf(t, formatted, paramName, ((List<char>)generatedValues[paramName])[i]),
             "System.String" => WhenListOf(t, formatted, paramName, ((List<string>)generatedValues[paramName])[i]),
-            "System.Nullable`1[System.Bool]" => WhenListOf(t, formatted, paramName, ((List<bool?>)generatedValues[paramName])[i]),
+            "System.Nullable`1[System.Boolean]" => WhenListOf(t, formatted, paramName, ((List<bool?>)generatedValues[paramName])[i]),
             "System.Nullable`1[System.Byte]" => WhenListOf(t, formatted, paramName, ((List<byte?>)generatedValues[paramName])[i]),
             "System.Nullable`1[System.Int16]" => WhenListOf(t, formatted, paramName, ((List<short?>)generatedValues[paramName])[i]),
             "System.Nullable`1[System.Int32]" => WhenListOf(t, formatted, paramName, ((List<int?>)generatedValues[paramName])[i]),
@@ -100,6 +102,7 @@ internal readonly record struct ParamValueTemplate
             "System.Nullable`1[System.Single]" => WhenListOf(t, formatted, paramName, ((List<float?>)generatedValues[paramName])[i]),
             "System.Nullable`1[System.Double]" => WhenListOf(t, formatted, paramName, ((List<double?>)generatedValues[paramName])[i]),
             "System.Nullable`1[System.Decimal]" => WhenListOf(t, formatted, paramName, ((List<decimal?>)generatedValues[paramName])[i]),
+            "System.Nullable`1[System.Char]" => WhenListOf(t, formatted, paramName, ((List<char?>)generatedValues[paramName])[i]),
             _ => formatted,
         };
     
--- tests/BrightSky.DataMock.Tests/MockTypeFormattedStringTests.cs
+++ tests/BrightSky.DataMock.Tests/MockTypeFormattedStringTests.cs
@@ -136,4 +136,330 @@ public class MockTypeFormattedStringTests
 
         Assert.Equal(expected, actual);
     }
+                
+    [Fact]
+    public void When_FormattedStringsToList_WithOneParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-A", 100).ToList();
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Chars().From(['A']))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void When_FormattedStringsToList_WithMultipleParams_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-A-B-C", 100).ToList();
+        var template = "template-{#p1}-{#p2}-{#p3}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Chars().From(['A']))
+            .Param("p2", () => Dm.Chars().From(['B']))
+            .Param("p3", () => Dm.Chars().From(['C']))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void When_FormattedStringsToList_Size_Negative_Throws_ArgumentOutOfRangeException()
+    {
+        var template = "template-{#p1}";
+        var action = () => Dm.FormattedStrings(template).ToList(-1);
+
+        Assert.Throws<ArgumentOutOfRangeException>(action);
+    }
+    
+    [Fact]
+    public void When_FormattedStringsToList_WithCharParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-A", 100).ToList();
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Chars().From(['A']))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+        
+    [Fact]
+    public void When_FormattedStringsToList_WithBoolParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-True", 100).ToList();
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Bools().TrueProbability(100))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+            
+    [Fact]
+    public void When_FormattedStringsToList_WithByteParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-1", 100).ToList();
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Bytes().Range(1, 1))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+                
+    [Fact]
+    public void When_FormattedStringsToList_WithShortParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-1", 100).ToList();
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Shorts().Range(1, 1))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+                    
+    [Fact]
+    public void When_FormattedStringsToList_WithIntParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-1", 100).ToList();
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Ints().Range(1, 1))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+                        
+    [Fact]
+    public void When_FormattedStringsToList_WithLongParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-1", 100).ToList();
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Longs().Range(1, 1))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+                            
+    [Fact]
+    public void When_FormattedStringsToList_WithFloatParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-0.1", 100).ToList();
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Floats().Range(0.1f, 0.1f))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+                                
+    [Fact]
+    public void When_FormattedStringsToList_WithDoubleParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-0.1", 100).ToList();
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Doubles().Range(0.1d, 0.1d))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+                                    
+    [Fact]
+    public void When_FormattedStringsToList_WithDecimalParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-0.1", 100).ToList();
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Decimals().Range(0.1m, 0.1m))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+                                        
+    [Fact]
+    public void When_FormattedStringsToList_WithStringParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-AAAAAAAAAA", 100).ToList();
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.Strings().From(['A']))
+            .ToList();
+
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void When_FormattedStringsToList_WithNullableBoolParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-True", 33)
+            .Concat(Enumerable.Repeat("template-False", 33))
+            .Concat(Enumerable.Repeat("template-", 34))
+            .Order()
+            .ToList();
+        
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.NullableBools())
+            .ToList()
+            .Order();
+
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void When_FormattedStringsToList_WithNullableByteParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-1", 50)
+            .Concat(Enumerable.Repeat("template-", 50))
+            .Order()
+            .ToList();
+        
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.NullableBytes().Range(1, 1))
+            .ToList()
+            .Order();
+
+        Assert.Equal(expected, actual);
+    }
+        
+    [Fact]
+    public void When_FormattedStringsToList_WithNullableShortParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-1", 50)
+            .Concat(Enumerable.Repeat("template-", 50))
+            .Order()
+            .ToList();
+        
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.NullableShorts().Range(1, 1))
+            .ToList()
+            .Order();
+
+        Assert.Equal(expected, actual);
+    }
+            
+    [Fact]
+    public void When_FormattedStringsToList_WithNullableIntParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-1", 50)
+            .Concat(Enumerable.Repeat("template-", 50))
+            .Order()
+            .ToList();
+        
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.NullableInts().Range(1, 1))
+            .ToList()
+            .Order();
+
+        Assert.Equal(expected, actual);
+    }
+                
+    [Fact]
+    public void When_FormattedStringsToList_WithNullableLongParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-1", 50)
+            .Concat(Enumerable.Repeat("template-", 50))
+            .Order()
+            .ToList();
+        
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.NullableLongs().Range(1, 1))
+            .ToList()
+            .Order();
+
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void When_FormattedStringsToList_WithNullableFloatParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-0.1", 50)
+            .Concat(Enumerable.Repeat("template-", 50))
+            .Order()
+            .ToList();
+        
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.NullableFloats().Range(0.1f, 0.1f))
+            .ToList()
+            .Order();
+
+        Assert.Equal(expected, actual);
+    }
+        
+    [Fact]
+    public void When_FormattedStringsToList_WithNullableDoubleParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-0.1", 50)
+            .Concat(Enumerable.Repeat("template-", 50))
+            .Order()
+            .ToList();
+        
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.NullableDoubles().Range(0.1d, 0.1d))
+            .ToList()
+            .Order();
+
+        Assert.Equal(expected, actual);
+    }
+            
+    [Fact]
+    public void When_FormattedStringsToList_WithNullableDecimalParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-0.1", 50)
+            .Concat(Enumerable.Repeat("template-", 50))
+            .Order()
+            .ToList();
+        
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.NullableDecimals().Range(0.1m, 0.1m))
+            .ToList()
+            .Order();
+
+        Assert.Equal(expected, actual);
+    }
+                
+    [Fact]
+    public void When_FormattedStringsToList_WithNullableCharParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-A", 50)
+            .Concat(Enumerable.Repeat("template-", 50))
+            .Order()
+            .ToList();
+        
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.NullableChars().From(['A']))
+            .ToList()
+            .Order();
+
+        Assert.Equal(expected, actual);
+    }
+                    
+    [Fact]
+    public void When_FormattedStringsToList_WithNullableStringParam_Returns_FormattedString_AsExpected()
+    {
+        var expected = Enumerable.Repeat("template-AAAAAAAAAA", 50)
+            .Concat(Enumerable.Repeat("template-", 50))
+            .Order()
+            .ToList();
+        
+        var template = "template-{#p1}";
+        var actual = Dm.FormattedStrings(template)
+            .Param("p1", () => Dm.NullableStrings().From(['A']))
+            .ToList()
+            .Order();
+
+        Assert.Equal(expected, actual);
+    }
 }
