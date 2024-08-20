b90afb9321546e5f2b631ea5dc2f7b887bd72d85
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-08-20 06:21:23 +0100

    Trying out an example way to use DataMock to create multiple unit tests from one single unit test.

---
+++ tests/BrightSky.DataMock.Tests/MockTheoryDataExamples.cs
@@ -0,0 +1,36 @@
+﻿namespace BrightSky.DataMock.Tests;
+
+public class MockTheoryDataExamples
+{
+    private class MockPrimitiveTheoryData : TheoryData<bool, int, double, string>
+    {
+        public MockPrimitiveTheoryData()
+        {
+            var p1Gen = Dm.Bools().ToList();
+            var p2Gen = Dm.Ints().ToList();
+            var p3Gen = Dm.Doubles().ToList();
+            var p4Gen = Dm.Strings().ToList();
+
+            for (var i = 0; i < p1Gen.Count; i++)
+                Add(p1Gen[i], p2Gen[i], p3Gen[i], p4Gen[i]);
+        }
+    }
+    
+    [Theory]  
+    [ClassData(typeof(MockPrimitiveTheoryData))]
+    public void Test_MockPrimitiveData_TheoryData(bool p1, int p2, double p3, string p4)
+    {
+        var anon = new
+        {
+            MyBool = p1,
+            MyInt = p2,
+            MyDouble = p3,
+            MyString = p4,
+        };
+
+        Assert.Equal(p1, anon.MyBool);
+        Assert.Equal(p2, anon.MyInt);
+        Assert.Equal(p3, anon.MyDouble);
+        Assert.Equal(p4, anon.MyString);
+    }    
+}
