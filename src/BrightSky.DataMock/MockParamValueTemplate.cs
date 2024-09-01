8e6dc424707b8135b9868220bcbbea62ded49bb2
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-09-01 20:44:05 +0100

    Swapping a switch expression for a dictionary plus linq.

--- src/BrightSky.DataMock/MockParamValueTemplate.cs
+++ src/BrightSky.DataMock/MockParamValueTemplate.cs
@@ -51,33 +51,41 @@ internal record MockParamValueTemplate(string Template)
 
         return generatedValues;
     }
-   
-    private static string Format(string? gta, Type t, string formatted, string paramName, Dictionary<string, object> generatedValues, int i)
-        => gta switch
+    
+    private static string Format(string gta, Type t, string formatted, string paramName, Dictionary<string, object> generatedValues, int i)
+    {
+        var dict = new Dictionary<Func<bool>, Func<string>>
         {
-            "System.Boolean" => WhenListOf(t, formatted, paramName, ((List<bool>)generatedValues[paramName])[i]),
-            "System.Byte" => WhenListOf(t, formatted, paramName, ((List<byte>)generatedValues[paramName])[i]),
-            "System.Int16" => WhenListOf(t, formatted, paramName, ((List<short>)generatedValues[paramName])[i]),
-            "System.Int32" => WhenListOf(t, formatted, paramName, ((List<int>)generatedValues[paramName])[i]),
-            "System.Int64" => WhenListOf(t, formatted, paramName, ((List<long>)generatedValues[paramName])[i]),
-            "System.Single" => WhenListOf(t, formatted, paramName, ((List<float>)generatedValues[paramName])[i]),
-            "System.Double" => WhenListOf(t, formatted, paramName, ((List<double>)generatedValues[paramName])[i]),
-            "System.Decimal" => WhenListOf(t, formatted, paramName, ((List<decimal>)generatedValues[paramName])[i]),
-            "System.Char" => WhenListOf(t, formatted, paramName, ((List<char>)generatedValues[paramName])[i]),
-            "System.String" => WhenListOf(t, formatted, paramName, ((List<string>)generatedValues[paramName])[i]),
-            "System.DateTime" => WhenListOf(t, formatted, paramName, ((List<DateTime>)generatedValues[paramName])[i]),
-            "System.Nullable`1[System.Boolean]" => WhenListOf(t, formatted, paramName, ((List<bool?>)generatedValues[paramName])[i]),
-            "System.Nullable`1[System.Byte]" => WhenListOf(t, formatted, paramName, ((List<byte?>)generatedValues[paramName])[i]),
-            "System.Nullable`1[System.Int16]" => WhenListOf(t, formatted, paramName, ((List<short?>)generatedValues[paramName])[i]),
-            "System.Nullable`1[System.Int32]" => WhenListOf(t, formatted, paramName, ((List<int?>)generatedValues[paramName])[i]),
-            "System.Nullable`1[System.Int64]" => WhenListOf(t, formatted, paramName, ((List<long?>)generatedValues[paramName])[i]),
-            "System.Nullable`1[System.Single]" => WhenListOf(t, formatted, paramName, ((List<float?>)generatedValues[paramName])[i]),
-            "System.Nullable`1[System.Double]" => WhenListOf(t, formatted, paramName, ((List<double?>)generatedValues[paramName])[i]),
-            "System.Nullable`1[System.Decimal]" => WhenListOf(t, formatted, paramName, ((List<decimal?>)generatedValues[paramName])[i]),
-            "System.Nullable`1[System.Char]" => WhenListOf(t, formatted, paramName, ((List<char?>)generatedValues[paramName])[i]),
-            "System.Nullable`1[System.DateTime]" => WhenListOf(t, formatted, paramName, ((List<DateTime?>)generatedValues[paramName])[i]),
-            _ => formatted,
+            { () => gta == "System.Boolean", () => WhenListOf(t, formatted, paramName, ((List<bool>)generatedValues[paramName])[i]) }, 
+            { () => gta == "System.Byte", () => WhenListOf(t, formatted, paramName, ((List<byte>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Int16", () => WhenListOf(t, formatted, paramName, ((List<short>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Int32", () => WhenListOf(t, formatted, paramName, ((List<int>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Int64", () => WhenListOf(t, formatted, paramName, ((List<long>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Single", () => WhenListOf(t, formatted, paramName, ((List<float>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Double", () => WhenListOf(t, formatted, paramName, ((List<double>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Decimal", () => WhenListOf(t, formatted, paramName, ((List<decimal>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Char", () => WhenListOf(t, formatted, paramName, ((List<char>)generatedValues[paramName])[i]) },
+            { () => gta == "System.String", () => WhenListOf(t, formatted, paramName, ((List<string>)generatedValues[paramName])[i]) },
+            { () => gta == "System.DateTime", () => WhenListOf(t, formatted, paramName, ((List<DateTime>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Nullable`1[System.Boolean]", () => WhenListOf(t, formatted, paramName, ((List<bool?>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Nullable`1[System.Byte]", () => WhenListOf(t, formatted, paramName, ((List<byte?>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Nullable`1[System.Int16]", () => WhenListOf(t, formatted, paramName, ((List<short?>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Nullable`1[System.Int32]", () => WhenListOf(t, formatted, paramName, ((List<int?>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Nullable`1[System.Int64]", () => WhenListOf(t, formatted, paramName, ((List<long?>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Nullable`1[System.Single]", () => WhenListOf(t, formatted, paramName, ((List<float?>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Nullable`1[System.Double]", () => WhenListOf(t, formatted, paramName, ((List<double?>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Nullable`1[System.Decimal]", () => WhenListOf(t, formatted, paramName, ((List<decimal?>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Nullable`1[System.Char]", () => WhenListOf(t, formatted, paramName, ((List<char?>)generatedValues[paramName])[i]) },
+            { () => gta == "System.Nullable`1[System.DateTime]", () => WhenListOf(t, formatted, paramName, ((List<DateTime?>)generatedValues[paramName])[i]) },
         };
+        
+        var matched = dict
+            .Where(kvp => kvp.Key())
+            .Select(kvp => kvp.Value())
+            .FirstOrDefault();
+        
+        return matched ?? formatted;
+    }
     
     private static string WhenListOf<T>(Type type, string formatted, string paramName, T value)
     {
