ed6cdaa949724546613817c9510b63ac0099be6b
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-09-07 15:58:40 +0100

    Started the impl. of Percentage type. Need to finish and then do unit tests to get complete coverage for it. All unit tests pass.

---
+++ src/BrightSky.DataMock/Percentage.cs
@@ -0,0 +1,18 @@
+﻿namespace BrightSky.DataMock;
+
+public record struct Percentage
+{
+    private readonly int _value = 0;
+
+    public Percentage(int value)
+    {        
+        if (value is < 0 or > 100)
+            throw new ArgumentOutOfRangeException(nameof(value), 
+                $"{nameof(value)} {value} must be a value between 0 to 100.");
+        
+        _value = value;
+    }
+    
+    public static implicit operator int(Percentage value) => value._value;
+    public static implicit operator Percentage(int value) => new(value);
+}
