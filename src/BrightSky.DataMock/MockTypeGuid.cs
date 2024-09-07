cf03141d6bbe6a1fcc85388c4c0f9a88b81573c5
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-09-07 09:08:47 +0100

    Adding to Guid functionality to now include Empty and NonEmpty probabilities. Still need to amend unit tests to now cover this. At the moment all unit tests pass.

--- src/BrightSky.DataMock/MockTypeGuid.cs
+++ src/BrightSky.DataMock/MockTypeGuid.cs
@@ -2,5 +2,30 @@
 
 public record MockTypeGuid : IMockType<Guid>
 {
-    public Guid Get() => Guid.NewGuid();
+    private readonly Random _random = new();
+
+    public int NonEmptyPercentage { get; private set; } = 50;
+    public int EmptyPercentage { get; private set; } = 50;
+    
+    public MockTypeGuid NonEmptyProbability(int nonEmptyPercentage)
+    {
+        (NonEmptyPercentage, EmptyPercentage) = CalculatePercentages(nameof(nonEmptyPercentage), nonEmptyPercentage);
+        return this;        
+    }
+
+    public MockTypeGuid EmptyProbability(int emptyPercentage)
+    {
+        (EmptyPercentage, NonEmptyPercentage) = CalculatePercentages(nameof(emptyPercentage), emptyPercentage);
+        return this;
+    }
+
+    private static (int FirstPrecentage, int SecondPrecentage) CalculatePercentages(string paramName, int percentage)
+    {
+        if (percentage is < 0 or > 100)
+            throw new ArgumentOutOfRangeException(paramName, $"{paramName} {percentage} must be a value from 0 to 100.");
+
+        return (percentage, 100 - percentage);
+    }
+    
+    public Guid Get() => _random.NextDouble() <= NonEmptyPercentage/100.0 ? Guid.NewGuid() : Guid.Empty;
 }
