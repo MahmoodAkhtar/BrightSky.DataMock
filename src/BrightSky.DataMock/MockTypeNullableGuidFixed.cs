01b5040e651073927eb70f82e914dbf3021cc485
MahmoodAkhtar <mahmood.akhtar@brightskyit.co.uk>
2024-09-16 06:57:30 +0100

    Adding new MockTypeNullableGuidFixed type. Not used yet. Need to add unit tests to get complete coverage. All unit tests pass.

---
+++ src/BrightSky.DataMock/MockTypeNullableGuidFixed.cs
@@ -0,0 +1,29 @@
+﻿namespace BrightSky.DataMock;
+
+internal record MockTypeNullableGuidFixed : 
+    IMockType<Guid?>,
+    IMockTypeNullableProbability<Guid?, MockTypeNullableGuidFixed>
+{
+    private readonly Guid? _fix;
+    
+    public MockTypeNullableGuidFixed(Guid fix) => _fix = fix;
+
+    public Guid? Get()
+    {
+        var chosen = new List<WeightedValue<Func<Guid?>>>
+        {
+            new(() => null, NullablePercentage),
+            new(() => _fix, 100 - NullablePercentage),
+        }.Next();
+        
+        return chosen();
+    }
+
+    public Percentage NullablePercentage { get; private set; } = (Percentage)50;
+
+    public MockTypeNullableGuidFixed NullableProbability(Percentage nullablePercentage)
+    {
+        NullablePercentage = nullablePercentage;
+        return this;
+    }
+}
